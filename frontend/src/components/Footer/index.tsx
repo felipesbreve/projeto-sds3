0f55d0f99da3904b7b4e04d76ded5d675976a432
Felipe Breve <brevefelipe@gmail.com>
2021-09-09 19:18:41 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/felipesbreve" target="_blank" rel="noreferrer">Felipe Breve</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior</small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
