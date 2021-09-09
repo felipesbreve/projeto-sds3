836812ce52e2cc204d6e8f682c35f3236eb2f485
Felipe Breve <brevefelipe@gmail.com>
2021-09-09 17:21:07 -0300

    Project created

---
+++ backend/src/main/java/com/felipebreve/fsbvendas/FsbvendasApplication.java
@@ -0,0 +1,13 @@
+package com.felipebreve.fsbvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsbvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsbvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/felipebreve/fsbvendas/FsbvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipebreve.fsbvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsbvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
