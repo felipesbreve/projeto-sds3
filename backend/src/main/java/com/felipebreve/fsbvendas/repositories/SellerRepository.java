6ba9d39b476a663ac76a10f60f21d53206fa00a1
Felipe Breve <brevefelipe@gmail.com>
2021-09-12 14:50:36 -0300

    Layers

---
+++ backend/src/main/java/com/felipebreve/fsbvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.felipebreve.fsbvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.felipebreve.fsbvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
---
+++ backend/src/main/java/com/felipebreve/fsbvendas/services/SellerService.java
@@ -0,0 +1,24 @@
+package com.felipebreve.fsbvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipebreve.fsbvendas.dto.SellerDTO;
+import com.felipebreve.fsbvendas.entities.Seller;
+import com.felipebreve.fsbvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+
+}
